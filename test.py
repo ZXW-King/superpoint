7e87453e8a8a0151e441a3f0c7023af02fc99312
张新卫 <zhangxinwei@indemind.cn>
2023-04-03 18:06:49 +0800

    rm test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-basedir = '/media/xin/data1/shujuji0310/小广场/高光/recordgcgaoguang500/imsee_data.bag.imgs.L/'
-img_glob = "*.png"
-search = os.path.join(basedir, img_glob)
-listing = glob.glob(search)
-listing.sort()
-print(listing)
